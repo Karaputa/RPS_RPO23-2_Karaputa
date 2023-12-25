4cb8f8eb461766c997bf107822808a000d652da9
RPO23/2 : Karaputa : ex:binary_search <s.karaputa2017@yandex.ru>
2023-12-25 20:34:04 +0300

    Karaputa_RPO23/2

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 import random
 
+choices = {'камень': 1, 'ножницы': 2, 'бумага': 3, 'ящирица': 4, 'спок': 5}
+reversed_choices = {v: k for k, v in choices.items()}
+
+
 def get_user_choice():
-    user_choice = input("Выберите: камень, ножницы или бумага? ").lower()
-    while user_choice not in ['камень', 'ножницы', 'бумага']:
-        print("Неверный ввод. Пожалуйста, выберите камень, ножницы или бумагу.")
-        user_choice = input("Выберите: камень, ножницы или бумага? ").lower()
-    return user_choice
+    user_input = input("Выберите камень, ножницы, бумага, ящирица, спок: ")
+
+    while user_input.lower() not in choices:
+        print("Неверный выбор. Попробуйте еще раз.")
+        user_input = input("Выберите камень, ножницы, бумага, ящирица, спок: ")
+
+    return choices[user_input.lower()]
 
 
 def get_computer_choice():
-    return random.choice(['камень', 'ножницы', 'бумага'])
+    pc_input = random.choice(list(choices.keys()))
+    return choices[pc_input]
+
 
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        return "Ничья!"
-    elif (
-            (user_choice == 'камень' and computer_choice == 'ножницы') or
-            (user_choice == 'ножницы' and computer_choice == 'бумага') or
-            (user_choice == 'бумага' and computer_choice == 'камень')
-    ):
-        return f"Вы выиграли!"
+        print("Ничья!")
+    elif (user_choice == 1 and computer_choice == 2) or \
+            (user_choice == 1 and computer_choice == 4) or \
+            (user_choice == 2 and computer_choice == 3) or \
+            (user_choice == 2 and computer_choice == 4) or \
+            (user_choice == 3 and computer_choice == 1) or \
+            (user_choice == 3 and computer_choice == 5) or \
+            (user_choice == 4 and computer_choice == 5) or \
+            (user_choice == 4 and computer_choice == 3) or \
+            (user_choice == 5 and computer_choice == 1) or \
+            (user_choice == 5 and computer_choice == 2):
+        print(f"Поздравляю, вы победили! Компьютер выбрал: {reversed_choices[computer_choice]}")
     else:
-        return f"Вы проиграли."
+        print(f"Вы проиграли! Компьютер выбрал: {reversed_choices[computer_choice]}")
 
 
-def game():
-    print("Добро пожаловать в игру 'камень, ножницы, бумага'!")
-
-    while True:
+while True:
+    a = input('вы желаете продолжить?')
+    if a.lower() == 'да':
         user_choice = get_user_choice()
         computer_choice = get_computer_choice()
-
-        print(f"Вы выбрали: {user_choice}")
-        print(f"Компьютер выбрал: {computer_choice}")
-
-        result = determine_winner(user_choice, computer_choice)
-        print(result)
-
-        play_again = input("Хотите сыграть еще раз? (да/нет) ").lower()
-        if play_again != 'да':
-            print("Спасибо за игру! До свидания.")
-            break
-
-game()
+        determine_winner(user_choice, computer_choice)
+    else:
+        break
